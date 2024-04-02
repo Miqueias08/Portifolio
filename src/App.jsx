f452871302d73a7cd9e77ba301ea832dc6df7743
Miqueias08 <miqueiasfernando@gmail.com>
2024-04-01 23:41:44 -0300

    correcao

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
+import React, { useEffect } from 'react';
 
 import Sidebar from './components/Sidebar';
 import MainContent from './components/MainContent';
 import './styles/components/app.sass';
 
 function App() {
+  useEffect(() => {
+    document.title = 'Miquéias Fernando Almeida de Jesus';
+  }, []);
+
   return (
     <div id="portifolio">
       <h1>Miquéias Fernando</h1>
       <Sidebar />
       <MainContent />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
